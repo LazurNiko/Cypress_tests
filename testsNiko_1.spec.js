1ea388e518f105fd42f953ceddb1841e0e507431
Niko Lazur <74565339+LazurNiko@users.noreply.github.com>
2021-11-18 17:46:58 +0300

    Add files via upload

---
+++ testsNiko_1.spec.js
@@ -0,0 +1,69 @@
+describe('', () => {
+  let user;
+
+  before(() => {
+    cy.task('newUser').then((newUser) => {
+      user = newUser;
+    });
+  });
+
+  it('', () => {
+    cy.visit('http://localhost:1667/#/');
+    cy.get(':nth-child(2) > .nav-link')
+      .click();
+    cy.get(':nth-child(1) > .form-control')
+      .type('Cypress@mail.com');
+    cy.get(':nth-child(2) > .form-control')
+      .type('12345Qwert!');
+    cy.get('.btn')
+      .click();
+    cy.get('.navbar')
+      .contains('Cypress')
+      .should('exist');
+  })
+})
+
+  let user;
+
+  before(() => {
+    cy.task('newUser').then((newUser) => {
+      user = newUser;
+    });
+  });
+
+  it('', () => {
+    cy.visit('http://localhost:1667/#/');
+    cy.get(':nth-child(3) > .nav-link')
+      .click();
+    cy.get(':nth-child(1) > .form-control')
+      .type(user.username);
+      cy.get(':nth-child(2) > .form-control')
+      .type(user.email);
+      cy.get(':nth-child(3) > .form-control')
+      .type(user.password);
+      cy.get('.btn')
+        .click();
+
+      cy.get('.swal-text')
+        .should('contain', 'Your registration was successful!');
+      cy.get('.swal-button')
+        .click();
+
+      cy.get(':nth-child(4) > .nav-link')
+        .should('contain', user.username);
+
+      cy.get(':nth-child(3) > .nav-link')
+        .click();
+      cy.get(':nth-child(1) > .form-control')
+        .should('exist');
+      cy.get(':nth-child(2) > .form-control')
+        .should('exist');
+      cy.get(':nth-child(3) > .form-control')
+        .should('exist');
+      cy.get(':nth-child(4) > .form-control')
+        .should('exist');
+      cy.get(':nth-child(5) > .form-control')
+        .should('exist');
+      cy.get('.btn-outline-danger')
+        .click();
+  })
